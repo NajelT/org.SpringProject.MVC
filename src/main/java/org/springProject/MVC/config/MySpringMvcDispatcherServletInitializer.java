9b345b8033f61381d2e7db809cb71495ca848719
Ilja <ilushpajev@gmail.com>
2022-04-04 16:21:41 +0300

    Controllers

---
+++ src/main/java/org/springProject/MVC/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package org.springProject.MVC.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class [] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
