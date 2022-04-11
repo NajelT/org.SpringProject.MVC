464961dfcd56d7a448cb870f473c0b634394d357
Ilja <ilushpajev@gmail.com>
2022-04-11 10:36:27 +0300

    My HttpRequest with parameters

--- src/main/java/org/springProject/MVC/controllers/FirstController.java
+++ src/main/java/org/springProject/MVC/controllers/FirstController.java
@@ -4,13 +4,20 @@ package org.springProject.MVC.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname){
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
